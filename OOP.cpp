41233c2ab8471f3f04b670075750b4d47cec505a
Rizhuycihuy <rizki003131@gmail.com>
2026-04-21 14:58:05 +0700

    add output

--- OOP.cpp
+++ OOP.cpp
@@ -4,7 +4,11 @@ using namespace std;
 class mahassiswa{
     public:
           int nim;
-          string nam;
+          string nama;
           float nilai;
-          
+          void printdata(){
+              cout <<"nim = "<< nim <<endl;
+              cout <<"nama = "<< nama <<endl;
+              cout <<"nilai = "<< nilai <<endl;
+          }
 };
